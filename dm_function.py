a69132b530249de9ca7c48d924f12c896a29fd4d
Amir Hossein Azizi <samaritan21@gmail.com>
2018-12-09 20:07:38 +0100

    Cloud coverage is added.

--- dm_function.py
+++ dm_function.py
@@ -7,6 +7,7 @@ Created on Wed Dec  5 16:19:39 2018
 """
 import ephem #used to calculate sun/moon rise/set. TODO: use novas_py or skyfield
 from datetime import date
+import requests
 
 def daymoon():
     bochum = ephem.Observer()
@@ -41,8 +42,19 @@ def daymoon():
 
     #TODO: make sure this sanity check works for all cases
     if dm_start != '-' and dm_end != '-':
-        out_message = 'Today daymoon is visible from %s to %s.' % (dm_start, dm_end)
+        out_message = 'Today daymoon is visible from %s to %s.\n' % (dm_start, dm_end)
     #    print(out_message)
     else:
         out_message = '--'
     return out_message
+
+def weather(token, lon, lat):
+    api_url = 'http://api.openweathermap.org/data/2.5/forecast?lat=%s&lon=%s&APPID=%s' % (lat, lon, token)
+    params = {'timeout': 30, 'offset': None}
+    resp = requests.get(api_url, params)
+    result_json = resp.json()['list'][0]['clouds']['all']
+    if result_json < 50:
+        message = 'Cloud coverage = %d' % result_json['all']
+    else:
+        message = '--'
+    return message
--- dm_sched.py
+++ dm_sched.py
@@ -13,6 +13,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from pytz import timezone
 
 bot_token = #ask me for it
+openW_api = #ask me for it
+#Bochum lon and lat
+lat = '51'
+lon = '7'
+
 bot = telebot.TeleBot(bot_token)
 #TODO: get the id from new starts
 chat_id = -355590034 #for the 'Daymmon-group' in Bochum
@@ -23,8 +28,10 @@ sched = BlockingScheduler()
 sched.configure(timezone=cest)
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=8)
 def scheduled_job():
-    out_message = dm_function.daymoon()
-    if out_message != '--':
-        bot.send_message(chat_id, out_message)
+    out_message1 = dm_function.daymoon()
+    if out_message1 != '--':
+        out_message2 = dm_function.weather(openW_api, lon, lat)
+        if out_message2 != '--':
+            bot.send_message(chat_id, out_message1+out_message2)
 
 sched.start()
